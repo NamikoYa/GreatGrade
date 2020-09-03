2414fbade8632a36f1690a2cbcef4231f7727eb0
Natascha Wernli <u113845@STRAUMANN.COM>
2020-09-03 12:26:05 +0200

    create db_connector

---
+++ src/db_connector.php
@@ -0,0 +1,14 @@
+<?php
+  $host = 'localhost'; // host
+  $username = 'GGUser'; // username
+  $password = 'GGPassword'; // password
+  $database = 'db_greatgrade'; // database
+
+  // connecto to database
+  $mysqli = new mysqli($host, $username, $password, $database);
+
+  // error message in case connection fails
+  if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);
+  }
+?>
