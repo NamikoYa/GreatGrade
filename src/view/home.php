a3a4afe1ff699939a6d52f2b8b6921e4ecdcd165
Natascha Wernli <u113845@STRAUMANN.COM>
2020-09-04 16:34:48 +0200

    added some content to the homepage

--- src/view/home.php
+++ src/view/home.php
@@ -1,3 +1,27 @@
 <div class="wrapper home">
+  <div class="center_div">
+    <div class="jumbotron">
+      <!-- TODO: Welcome Firstname Lastname, dynamically -->
+      <h1 class="display-4">Welcome Donald Duck!</h1>
+      <p class="lead">This is a simple application for administering and viewing grades and content.</p>
+      <hr class="my-4">
+      <p class="lead">Visit your pages:</p>
+      <p class="lead">
+        <a class="btn btn-success btn-lg" href="?view=overview" role="button">Overview - Grades & Content</a>
+        <a class="btn btn-dark btn-lg" href="?view=settings" role="button">Settings - Profile & Customization</a>
+      </p>
+    </div>
 
+    <div class="jumbotron">
+      <h3>New here?</h3>
+      <p class="lead">
+        Don't worry, here is a quick guide for you. If you still need help or have questions please contact one
+        of our administrators. <br/><br/>
+        To view your grades per subject please go to <a class='links' href="?view=settings">My Settings</a>. To view your profile or change your password go
+        to 'My Settings'. <br/><br/>
+        This application is still quite new so new features will be added and bugs will be fixed, just be patient. <br/>
+        If you have any ideas on making <b>GreatGrade</b> better, please contact one of our administrators. Thank you!
+      </p>
+    </div>
+  </div>
 </div>
