2e46af497e5f8a55995bcca3a850012b02730c5e
Natascha Wernli <u113845@STRAUMANN.COM>
2020-09-07 07:28:46 +0200

    small text changes

--- src/view/home.php
+++ src/view/home.php
@@ -17,8 +17,8 @@
       <p class="lead">
         Don't worry, here is a quick guide for you. If you still need help or have questions please contact one
         of our administrators. <br/><br/>
-        To view your grades per subject please go to <a class='links' href="?view=settings">My Settings</a>. To view your profile or change your password go
-        to 'My Settings'. <br/><br/>
+        To view your grades per subject please go to <a class='links' href="?view=overview">Overview</a>. To view your profile or change your password go
+        to <a class='links' href="?view=settings">My Settings</a>. <br/><br/>
         This application is still quite new so new features will be added and bugs will be fixed, just be patient. <br/>
         If you have any ideas on making <b>GreatGrade</b> better, please contact one of our administrators. Thank you!
       </p>
