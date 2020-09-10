686d43ec61e03339a3a933ca340159a459adda50
Natascha Wernli <u113845@STRAUMANN.COM>
2020-09-10 08:57:03 +0200

    added overview, changes dynamically, one issue

--- src/view/overview.php
+++ src/view/overview.php
@@ -1,2 +1,80 @@
+<?php
+// TODO: How to change variable in select option? Changes dynamically already -> do not change
+$subject = '';
+?>
+
 <div class="wrapper overview">
+
+  <div class="center_div">
+
+    <!-- Grades Panel -->
+    <div class="card text-white bg-dark mb-3">
+      <h5 class="card-header">Grade Overview</h5>
+      <div class="card-body">
+        <!-- Select for subject -->
+        <label class="mr-sm-2" for="inlineFormCustomSelect">Subject</label>
+        <select name="subjectSelect" class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+          <option disabled>Choose...</option>
+          <!-- Get subject options from database -->
+          <?php
+          $query = "SELECT * FROM tbl_subjects";
+          $stmt = $mysqli->prepare($query);
+          $stmt->execute();
+          $result=$stmt->get_result();
+          
+          if($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()){
+              echo '<option>' . $row['subject'] . '</option>';
+            }
+          }
+          $result->free();
+          ?>
+        </select>
+        <hr class="my-4 bg-light">
+        <table class="table table-dark">
+          <caption>List of grades</caption>
+          <thead>
+            <!-- TODO: Teacher can edit according to subject, can view everything -->
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            if($subject != '') {
+              $query = "SELECT u.firstname, u.lastname, s.subject, g.grade FROM tbl_grades as g INNER JOIN tbl_users as u on g.studentID = u.ID INNER JOIN tbl_subjects as s on g.subjectID = s.ID where s.subject = ? order by u.lastname";
+              $stmt = $mysqli->prepare($query);
+              $stmt->bind_param("s", $subject);
+              $stmt->execute();
+              $result=$stmt->get_result();
+
+              if($result->num_rows > 0) {
+                $count = 0;
+                while($row = $result->fetch_assoc()){
+                  if(($count % 2) != 0) {
+                    echo '<tr class="bg-success">';
+                  } else {
+                    echo '<tr>';
+                  }
+                  echo '<th scope="row">' . ($count + 1) . '</th>';
+                  echo '<td>' . $row['firstname'] . '</td>';
+                  echo '<td>' . $row['lastname'] . '</td>';
+                  echo '<td>' . $row['grade'] . '</td>';
+                  echo '</tr>';
+                  $count++;
+                }
+              }
+              $result->free();
+            }
+            ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+  </div>
+
 </div>
