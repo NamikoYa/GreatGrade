cfdbf4dfc38be1dc90e4ae0987553a3c6c8959b1
Natascha Wernli <u113845@STRAUMANN.COM>
2020-09-11 15:50:39 +0200

    created new folder and script

---
+++ src/js/myscript.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#edit").click(function() {
+    // only works for first line? Why?
+    $(this).parents("tr").find("td:nth-child(2)").each(function(){
+      let first = this.innerText;
+      let last = this.nextSibling.innerText;
+			console.log(first, " ", last);
+		});
+  });
+});
