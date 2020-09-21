5a2d55bbc0b8986c5eef62edf530d84c4b4892f9
Natascha Wernli <u113845@STRAUMANN.COM>
2020-09-21 08:23:00 +0200

    compacted code

--- src/php/delete_modal.php
+++ src/php/delete_modal.php
@@ -6,20 +6,13 @@ $current_user = '';
 if(isset($_POST['delete_user'])) {
   $confirmation = $_POST['confirmation'];
   $current_user = $_POST['select_user'];
-  if($confirmation != ('I ' . $username . ' agree')) {
+  if($confirmation != ('I ' . $username . ' agree') || empty($current_user)) {
     $error = 'Could not delete user.';
   }
   // if no error, delete user
   if(empty($error)) {
-    try {
-      $query = 'DELETE FROM tbl_users WHERE username = ?';
-      $stmt = $mysqli->prepare($query);
-      $stmt->bind_param('s', $current_user);
-      $stmt->execute();
-    } catch(Exeption $e) {
-      error_log($e->getMessage());
-      $error = 'Could not delete user';
-    }
+    $query = 'DELETE FROM tbl_users WHERE username = ?';
+    if(!$mysqli -> query($query)) $error = 'Could not delete user';
     if (empty($error)) {
       $message =  "User successfully deleted.";
       $mysqli->close();
