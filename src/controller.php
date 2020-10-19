e9479b9494289026ab1a8d30fc4e5930f4f321df
NamikoYa <nati.wernli@gmail.com>
2020-10-19 15:17:10 +0200

    updated controller, deleted prepared model

--- src/controller.php
+++ src/controller.php
@@ -1,6 +1,10 @@
 <?php
 
-include '../model/board.class.php';
+// the model would be included here
+// due to the small size of the project we decided to not overcomplicate it and didn't implement a model
+
+// include database connection
+include '../db_connector.php';
 
 $view = isset($_GET['view']) ? $_GET['view'] : '';
 
