9ccc204031f9d862a9d264dc25c23dc77bf9b0ae
0saRaa0 <65237206+0saRaa0@users.noreply.github.com>
2020-10-20 06:22:06 +0200

    bug fix

--- src/page/view/modals/edit_modal.php
+++ src/page/view/modals/edit_modal.php
@@ -8,7 +8,7 @@ if(isset($_POST['btn_save_grade'])) {
 	$current_user = $_POST['select_table_user'];
 	$data_grade = $_POST['grade'];
 	$grade = trim(htmlspecialchars($lv_firstname));
-	if(!preg_match('(.*[1-6]{1}\.[0-9]{1,2})', $grade)) $error = 'Could not update grade.'
+	if(!preg_match('(.*[1-6]{1}\.[0-9]{1,2})', $grade)) $error = 'Could not update grade.';
 	if(empty($error)) {
 		$query = "SELECT DISTINCT g.studentID FROM tbl_grades as g INNER JOIN tbl_users as u on g.studentID = u.ID INNER JOIN tbl_subjects as s on g.subjectID = s.ID where u.username = ?";
 		if(!$stmt = $mysqli->prepare($query)) $error = 'Could not update grade.';
@@ -28,11 +28,10 @@ if(isset($_POST['btn_save_grade'])) {
 			if(!$stmt->bind_param('di', $grade, $user_id)) $error = 'Could not update grade.';
 			if(!$stmt->execute()) $error = 'Could not update grade.';
 		}
-		
+	}	
     if (empty($error)) {
       $message =  "Grade successfully updated.";
       $mysqli->close();
     }
-  }
 }
 ?>
